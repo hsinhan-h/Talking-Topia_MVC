b7cedfb891d4de92ed05ba2b1f0c559970d5a91e
Chiaki <dark0423@hotmail.com>
2024-08-17 11:39:01 +0800

    feat: MemberCenterHomePage

--- Web/Controllers/MemberController.cs
+++ Web/Controllers/MemberController.cs
@@ -16,5 +16,9 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult MemberCenterHomePage()
+        {
+            return View();
+        }
     }
 }
