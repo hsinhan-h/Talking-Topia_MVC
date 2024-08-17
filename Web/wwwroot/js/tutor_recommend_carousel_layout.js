d317e4f44ddacea990853fa9ad716f3bbd77b06e
Chiaki <dark0423@hotmail.com>
2024-08-17 21:02:23 +0800

    調整關注列表JS、CSS及image路徑

---
+++ Web/wwwroot/js/tutor_recommend_carousel_layout.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    const tutorRecommendCarousel = $(this).find(
+        ".tutor-recommend-carousel-wrapper"
+    );
+    const arrowPrev = tutorRecommendCarousel.find(".arrow_prev");
+    const arrowNext = tutorRecommendCarousel.find(".arrow_next");
+    $(".tutor-recommend-carousel").slick({
+        // dots: true,
+        infinite: true,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
+        prevArrow: arrowPrev,
+        nextArrow: arrowNext,
+    });
+});
+
