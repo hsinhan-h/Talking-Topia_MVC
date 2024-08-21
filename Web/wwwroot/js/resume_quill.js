d74ecf532c1f1533e89cd3c3c4ff2b614dc5eb46
Eric <az710053@gmial>
2024-08-21 17:33:17 +0800

    修正富文本功能bug

--- Web/wwwroot/js/resume_quill.js
+++ Web/wwwroot/js/resume_quill.js
@@ -1,20 +1,19 @@
 ﻿const editors = document.querySelectorAll(".editor_resume");
 editors.forEach((element) => {
+    element.style.height = "300px"; // 設定編輯器容器的高度
     new Quill(element, {
         theme: 'snow',
         placeholder: '輸入...',
         modules: {
             toolbar: [
-                [{ 'size': ['small', false, 'large', 'huge'] }],
-                [{ 'header': [1, 2, 3, false] }], 
+                [{ 'header': [1, 2, 3, false] }],
                 ['bold', 'underline'],
-                [{ 'color':[]}, { 'background': [] }], 
-                [{ 'list': 'ordered' }, { 'list': 'bullet' }], 
-                [{ 'align': [] }], 
+                [{ 'color': [] }, { 'background': [] }],
+                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                [{ 'align': [] }],
                 [{ 'indent': '-1' }, { 'indent': '+1' }],
-                ['link', 'image', 'video'] 
+                ['link', 'image', 'video']
             ]
         }
     });
 });
-
