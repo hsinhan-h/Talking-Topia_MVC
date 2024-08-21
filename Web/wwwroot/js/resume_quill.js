480dfa8d28ae4811cee389a8f8b3cfb3e56a7524
Eric <az710053@gmial>
2024-08-21 12:28:05 +0800

    修改富文本功能

--- Web/wwwroot/js/resume_quill.js
+++ Web/wwwroot/js/resume_quill.js
@@ -1,7 +1,20 @@
 ﻿const editors = document.querySelectorAll(".editor_resume");
 editors.forEach((element) => {
     new Quill(element, {
-        theme: 'snow'
+        theme: 'snow',
+        placeholder: '輸入...',
+        modules: {
+            toolbar: [
+                [{ 'size': ['small', false, 'large', 'huge'] }],
+                [{ 'header': [1, 2, 3, false] }], 
+                ['bold', 'underline'],
+                [{ 'color':[]}, { 'background': [] }], 
+                [{ 'list': 'ordered' }, { 'list': 'bullet' }], 
+                [{ 'align': [] }], 
+                [{ 'indent': '-1' }, { 'indent': '+1' }],
+                ['link', 'image', 'video'] 
+            ]
+        }
     });
 });
 
