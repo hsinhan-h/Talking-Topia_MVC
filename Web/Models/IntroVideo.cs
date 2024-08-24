fd399dee9da9462e5138345095f5149919d544d0
Hsin Han <hsinhan831102@gmail.com>
2024-08-24 17:48:02 +0800

    delete obsolete model files

--- Web/Models/IntroVideo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Web.Models
-{
-    public class IntroVideo
-    {
-        public int Id { get; set; }
-        public string Thumbnail { get; set; }
-        public string VideoUrl { get; set; }
-    }
-}
