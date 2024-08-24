c535314ee1bd7d4c02d9e701e9beb4a198d7094c
Chiaki <dark0423@hotmail.com>
2024-08-24 23:13:28 +0800

    feat: orderservice

---
+++ Web/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Web.ViewModels;
+
+namespace Web.Services
+{
+    public class OrderService
+    {
+        public async Task<BookingListViewModel> GetOrderList()
+        { 
+        
+        var order = new List<OrderManagementViewModel>
+        {
+            new OrderManagementViewModel
+            { 
+                HeadShotImage ="",
+                FullName="",
+                WorkExperience="",
+                CourseTitle="",
+                CourseSubject="",
+                CourseCategory="",
+                CourseLength="",
+                CourseQuantity=10,
+                UnitPrice=800,
+                SubtotalNTD=8000,
+                Coupon="",
+                PaymentType="",
+                TaxIdNumber="",
+                OrderDatetime="",
+                OrderStatus="",
+                BookingDate=2024/8/24,
+                BookingTime=2307,
+            }
+        };
+        }
+    }
+}
