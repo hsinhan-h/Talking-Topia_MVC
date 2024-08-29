976139a03fa34629d3bdb8582e890aaaa38a1a92
Chiaki <dark0423@hotmail.com>
2024-08-29 20:40:25 +0800

    feat: TutorController

---
+++ Web/Controllers/TutorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Services;
+
+namespace Web.Controllers
+{
+    public class TutorController : Controller
+    {
+        private readonly ResumeDataService _resumeDataService;
+        public TutorController(ResumeDataService resumeDataService)
+        {
+            _resumeDataService = resumeDataService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Toteacher()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ToteacherResume()
+        {
+            var ResumeSummaryData = await _resumeDataService.GetResumeData("tommy85");
+            return View(ResumeSummaryData);
+        }
+        public IActionResult RecommendedTeachersAI()
+        {
+            return View();
+        }
+    }
+}
