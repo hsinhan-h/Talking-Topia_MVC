f0fe4f7a50acf783dcedd7d22d3d1af09f06aca0
XieLiLun <don21471105@gmail.com>
2024-08-30 17:28:32 +0800

    add watchListViewModel

---
+++ Web/ViewModels/WatchListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.ViewModels
+{
+    public class WatchListViewModel
+    {
+        public List<WatchListViewModel> WatchList { get; set; }
+    }
+    public class WatchViewModel
+    {
+        public string Name { get; set; }
+    }
+}
