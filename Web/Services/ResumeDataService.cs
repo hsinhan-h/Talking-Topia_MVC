2dd2dcbf04bc473567ef8988664fc9fbda3f8183
Eric <az710053@gmial>
2024-09-02 01:32:28 +0800

    修改TutorResume Service

--- Web/Services/ResumeDataService.cs
+++ Web/Services/ResumeDataService.cs
@@ -13,16 +13,24 @@ namespace Web.Services
         }
         public async Task<TutorResumeListViewModel> GetEducationAsync(int memberId)
         {
-            var resumeValues = await _repository.GetAll<Education>()
-                .Where(e => e.EducationId == memberId) 
-                .Select(r => new TutorResumeViewModel
-                {
-                    SchoolName = r.SchoolName
-                }).ToListAsync();
+            var resumeValues = from e in _repository.GetAll<Education>()
+                               join member in _repository.GetAll<Member>() on e.EducationId equals member.EducationId
+                               join w in _repository.GetAll<WorkExperience>() on member.MemberId  equals w.MemberId
+                               where member.MemberId == 2
+                               select new TutorResumeViewModel
+                               {
+                                   SchoolName = e.SchoolName,
+                                   StudyEndYear = e.StudyEndYear,
+                                   StudyStartYear = e.StudyStartYear,
+                                   DepartmentName = e.DepartmentName,
+                                   WorkEndDate = w.WorkEndDate,
+                                   WorkStartDate = w.WorkStartDate,
+                                   WorkName= w.WorkName,
+                               };
 
             return new TutorResumeListViewModel()
             {
-                TutorResumeList = resumeValues,
+                TutorResumeList = await resumeValues.ToListAsync(),
             };
         }
     }
