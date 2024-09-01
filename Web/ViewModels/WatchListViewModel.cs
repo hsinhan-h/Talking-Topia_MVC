07f54d623dd74ebfefb289a53ae70ecf0d18dc07
XieLiLun <don21471105@gmail.com>
2024-09-02 00:27:09 +0800

    add watchLisVm

--- Web/ViewModels/WatchListViewModel.cs
+++ Web/ViewModels/WatchListViewModel.cs
@@ -6,6 +6,32 @@
     }
     public class WatchViewModel
     {
-        public string Name { get; set; }
+        /// <summary>
+        /// 教師頭像
+        /// </summary>
+        public string HeadShotImage { get; set; }
+        /// <summary>
+        /// 課程名稱
+        /// </summary>
+        public string CourseTitle { get; set; }
+
+        /// <summary>
+        /// 課程科目
+        /// </summary>
+        public string CourseSubject { get; set; }
+
+        /// <summary>
+        /// 教師簡介
+        /// </summary>
+        public string TutorIntro { get; set; }
+        /// <summary>
+        /// 25分鐘價格
+        /// </summary>
+        public int TwentyFiveMinPriceNTD { get; set; }
+        /// <summary>
+        /// 50分鐘價格
+        /// </summary>
+        public int FiftyMinPriceNTD { get; set; }
+
     }
 }
