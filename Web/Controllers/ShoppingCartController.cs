978be87fbf6f48ed2bed69555f0fbe37d19bc918
Chiaki <dark0423@hotmail.com>
2024-09-01 15:37:49 +0800

    調整: ShoppingCart的Controller等檔案名稱一致

--- Web/Controllers/ShoppingCartController.cs
+++ Web/Controllers/ShoppingCartController.cs
@@ -13,10 +13,24 @@ namespace Web.Controllers
         /// 原ShoppingCartCheck
         /// </summary>
         /// <returns></returns>
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<IActionResult> Index(int memberId)
         {
-            var shoppingCartData = await _shoppingCartService.GetShoppingCartCheckList();
-            return View(shoppingCartData);
+            //初次進購物車tempCart為空
+            //todo: 先確認ShoppingCart是否有資料
+            //todo: 無資料 - create路線 - get/post
+            //todo: 有資料 - read路線 - post
+
+            //todo: 
+            if (memberId > -1)
+            {
+                var shoppingCartData = await _shoppingCartService.GetShoppingCartCheckList();
+                return View(shoppingCartData);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
