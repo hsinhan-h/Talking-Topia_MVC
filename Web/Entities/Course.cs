2cd4d4298c6da2cc69f53cbd4ad74bbe0e61629a
Allen Chang <shang_lun@hotmail.com>
2024-09-01 15:33:33 +0800

    修改Entities: 1.加上Smmary 2.CourseCategory修改名稱

--- Web/Entities/Course.cs
+++ Web/Entities/Course.cs
@@ -5,39 +5,84 @@ namespace Web.Entities;
 
 public partial class Course
 {
+    /// <summary>
+    /// 課程Id
+    /// </summary>
     public int CourseId { get; set; }
 
+    /// <summary>
+    /// 課程類別Id
+    /// </summary>
     public int CategoryId { get; set; }
 
+    /// <summary>
+    /// 科目Id
+    /// </summary>
     public int SubjectId { get; set; }
 
+    /// <summary>
+    /// 學生Id
+    /// </summary>
     public int TutorId { get; set; }
 
+    /// <summary>
+    /// 課程標題
+    /// </summary>
     public string Title { get; set; }
 
+    /// <summary>
+    /// 課程副標題
+    /// </summary>
     public string SubTitle { get; set; }
 
+    /// <summary>
+    /// 25分鐘價
+    /// </summary>
     public decimal TwentyFiveMinUnitPrice { get; set; }
 
+    /// <summary>
+    /// 50分鐘價
+    /// </summary>
     public decimal FiftyMinUnitPrice { get; set; }
 
+    /// <summary>
+    /// 課程詳細描述
+    /// </summary>
     public string Description { get; set; }
 
+    /// <summary>
+    /// 是否顯示
+    /// </summary>
     public bool IsEnabled { get; set; }
 
+    /// <summary>
+    /// 影片封面
+    /// </summary>
     public string ThumbnailUrl { get; set; }
 
+    /// <summary>
+    /// 影片路徑
+    /// </summary>
     public string VideoUrl { get; set; }
 
+    /// <summary>
+    /// 課程審核狀態
+    /// </summary>
     public short CoursesStatus { get; set; }
 
+    /// <summary>
+    /// 建立時間
+    /// </summary>
     public DateTime Cdate { get; set; }
 
+    /// <summary>
+    /// 修改時間
+    /// </summary>
     public DateTime? Udate { get; set; }
 
     public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 
-    public virtual ICollection<CourseCategorite> CourseCategorites { get; set; } = new List<CourseCategorite>();
+    public virtual ICollection<CourseCategory> CourseCategories { get; set; } = new List<CourseCategory>();
 
     public virtual ICollection<CourseImage> CourseImages { get; set; } = new List<CourseImage>();
 
--- Web/Entities/CourseSubject.cs
+++ Web/Entities/CourseSubject.cs
@@ -5,17 +5,32 @@ namespace Web.Entities;
 
 public partial class CourseSubject
 {
+    /// <summary>
+    /// 課程科目Id
+    /// </summary>
     public int SubjectId { get; set; }
 
+    /// <summary>
+    /// 課程科目名稱
+    /// </summary>
     public string SubjectName { get; set; }
 
+    /// <summary>
+    /// 課程類別Id
+    /// </summary>
     public int CourseCategoryId { get; set; }
 
+    /// <summary>
+    /// 建立時間
+    /// </summary>
     public DateTime Cdate { get; set; }
 
+    /// <summary>
+    /// 更新時間
+    /// </summary>
     public DateTime? Udate { get; set; }
 
-    public virtual CourseCategorite CourseCategory { get; set; }
+    public virtual CourseCategory CourseCategory { get; set; }
 
     public virtual ICollection<MemberPreference> MemberPreferences { get; set; } = new List<MemberPreference>();
 }
