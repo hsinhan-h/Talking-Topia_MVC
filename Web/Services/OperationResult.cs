cb4a34956875f7a54fbc316a005397f30847a3c5
Chiaki <dark0423@hotmail.com>
2024-09-07 23:46:31 +0800

    feat: Service - OperationResult (forErrorLog.txt)

---
+++ Web/Services/OperationResult.cs
@@ -0,0 +1,26 @@
+﻿namespace Web.Services
+{
+    public class OperationResult
+    {
+        public bool IsSuccessful { get; set; }
+        public Exception Exception { get; set; }
+    }
+    public static class OperationResultHelper
+    {
+        public static string WriteLog(this OperationResult value)
+        {
+            if (value.Exception != null)
+            {
+                string path = DateTime.Now.ToString("yyyy-MM-dd_HH_mm_ss");
+                path = path + ".txt";
+                File.WriteAllText(path, value.Exception.ToString());
+                return path;
+            }
+            else
+            {
+                return "沒有存檔";
+            }
+        }
+    }
+
+}
