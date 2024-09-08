3ae43a87739101b0b7d3d78052059d4004ff4a81
Chiaki <dark0423@hotmail.com>
2024-09-08 18:57:19 +0800

    調整: ShoppingCartController跳轉頁面的判斷

--- Web/Controllers/ShoppingCartController.cs
+++ Web/Controllers/ShoppingCartController.cs
@@ -14,7 +14,6 @@ namespace Web.Controllers
         }
         /// <summary>
         /// ShoppingCart頁面
-        /// Course頁面應回傳時長(value設定為25或50)與堂數(value設定堂數)
         /// 設計一個VM接收option回傳的值(asp-for="對應value的VM欄位")
         /// </summary>
         public async Task<IActionResult> Index([FromQuery] int memberId)
@@ -25,14 +24,17 @@ namespace Web.Controllers
                 ShoppingCartList = cartData
             };
             return View(result);
-
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddToCart([FromForm] int memberId, [FromForm] int courseId, [FromForm] int courseLength, [FromForm] int quantity)
         {
+            if (!_shoppingCartService.HasMemberData(memberId))
+            { return RedirectToAction(nameof(HomeController.Account), "Home"); }
+            if (!_shoppingCartService.HasCourseData(courseId))
+            { return RedirectToAction(nameof(HomeController.Index), "Home", new { memberId }); }
             var cartData = await _shoppingCartService.GetShoppingCartData(memberId, courseId, courseLength, quantity);
-            return RedirectToAction(nameof(Index), new { memberId });
+            return RedirectToAction(nameof(Index), "ShoppingCart", new { memberId });
         }
     }
 }
