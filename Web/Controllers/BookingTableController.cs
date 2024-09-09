82bb16241e927e5e0ba56acdf9b96ffde6c9079c
Hsin Han <hsinhan831102@gmail.com>
2024-09-09 22:41:47 +0800

    BookingTableController (todo1: 剩餘堂數 > 0, 允許預約)

--- Web/Controllers/BookingTableController.cs
+++ Web/Controllers/BookingTableController.cs
@@ -12,18 +12,23 @@ namespace Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult AddToCart(int CourseId, DateTime BookingDate, int BookingTime)
+        public async Task<IActionResult> AddToCart(int CourseId, DateTime BookingDate, int BookingTime)
         {
 
-            Console.WriteLine(BookingTime);
             // todo: 剩餘堂數 > 0, 允許預約
             // 寫入booking資料表, 導向預約成功頁面
-            //var remainSessions = await _bookingService.GetRemainCourseQtyAsync(1, CourseId);
+            //MemberId先暫時用1帶入
+            int remainSessions = await _bookingService.GetRemainCourseQtyAsync(1, CourseId);
+            if (remainSessions > 0)
+            {
+                _bookingService.CreateBookingData(CourseId, BookingDate, BookingTime, 1);
+                return RedirectToAction("Success", "BookingTable");
+            }
 
             //todo: 剩餘堂數 <= 0, 跳轉購物車, 將課程寫入購物車資料表
 
-
             return RedirectToAction("Success", "BookingTable");
+
         }
 
 
