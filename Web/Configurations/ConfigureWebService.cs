d3b31382b32a8d70d506d76a878cff381d078a08
Chiaki <dark0423@hotmail.com>
2024-09-12 23:49:38 +0800

    feat: Configurations

---
+++ Web/Configurations/ConfigureWebService.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Web.Services;
+
+namespace Web.Configurations
+{
+    public static class ConfigureWebService
+    {
+        public static IServiceCollection AddWebService(this IServiceCollection services)
+        {
+            
+            //services.AddScoped<IProductViewModelService, ProductViewModelService>(); 參考用
+            
+            return services;
+        }
+    }
+}
