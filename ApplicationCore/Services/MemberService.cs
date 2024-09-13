d36f8b44a5efcb18fff994cc35dfcc2e3ea40da8
Chiaki <dark0423@hotmail.com>
2024-09-13 20:34:03 +0800

    feat: IMberService & ICourseService及其實作

---
+++ ApplicationCore/Services/MemberService.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class MemberService : IMemberService
+    {
+        private readonly IRepository<Member> _memberRepository;
+
+        public MemberService(IRepository<Member> memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        public bool IsMember(int memberId)
+        {
+            return _memberRepository.Any(m => m.MemberId == memberId);
+        }
+    }
+}
