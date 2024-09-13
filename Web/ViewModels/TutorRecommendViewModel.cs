6d10a5855767292da52092f2d755c9e0de5e526d
Michael <m051412369811@gmail.com>
2024-09-13 17:31:08 +0800

    相似教師推薦ViewModel , Service完成

---
+++ Web/ViewModels/TutorRecommendViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Web.ViewModels
+{
+    public class TutorRecommendCardViewModel
+    {
+        public int MemberId { get; set; }
+        public int SubjectId { get; set; }
+        public int CategoryId { get; set; }
+        public List<TutorRecomCardList> TutorReconmmendCard { get; set; } 
+    }
+
+    public class TutorRecomCardList
+    {        
+        public int CourseId { get; set; }
+        public string TutorHeadShot { get; set; }
+        public string NationFlagImg { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseSubTitle { get; set; }
+        public int TwentyFiveMinPrice { get; set; }
+        public int FiftyminPrice { get; set; }
+        public double Rating { get; set; }
+        public string Description   { get; set; }
+    }
+}
