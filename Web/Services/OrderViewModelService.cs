33cb5e1b5b65d63b4453d1550f2c2f822de2ea95
Chiaki <dark0423@hotmail.com>
2024-09-13 18:02:02 +0800

    調整: OrderViewModelService

---
+++ Web/Services/OrderViewModelService.cs
@@ -0,0 +1,14 @@
+﻿using ApplicationCore.Interfaces;
+
+namespace Web.Services
+{
+    public class OrderViewModelService
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderViewModelService(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+    }
+}
