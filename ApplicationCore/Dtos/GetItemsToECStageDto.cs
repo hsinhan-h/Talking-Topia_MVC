f2118578959c6b1a55cf0f7b4e72939856e312aa
Chiaki <dark0423@hotmail.com>
2024-09-14 17:31:08 +0800

    feat: Dtos - GetItemToECStageDto

---
+++ ApplicationCore/Dtos/GetItemsToECStageDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ApplicationCore.Dtos
+{
+    public class GetItemsToECStageDto
+    {
+        public List<GetItemToECStage> GetItemsToECStage { get; set; }
+    }
+
+    public class GetItemToECStage
+    {
+        public string CourseName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public short CourseType { get; set; }
+    }
+}
