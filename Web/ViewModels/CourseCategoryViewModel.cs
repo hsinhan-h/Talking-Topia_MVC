3797cd7eb8bf0df64decc79d52bafb7d4a1b8ac6
Hsin Han <hsinhan831102@gmail.com>
2024-09-16 01:47:12 +0800

    新增CourseCategoryViewModel

---
+++ Web/ViewModels/CourseCategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Web.ViewModels
+{
+    public class CourseCategoryViewModel
+    {
+        public int CourseCategoryId { get; set; }
+        /// <summary>
+        /// 課程種類
+        /// </summary>
+        public string CategoryName { get; set; }
+        /// <summary>
+        /// 課程科目
+        /// </summary>
+        public List<CourseSubjectViewModel> Subjects { get; set; }
+    }
+
+    public class CourseSubjectViewModel
+    {
+        public string SubjectName { get; set; }
+    }
+}
