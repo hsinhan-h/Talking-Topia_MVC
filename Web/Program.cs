787e85c00715e69dbc109b8eda29586123c97dac
Allen Chang <shang_lun@hotmail.com>
2024-09-16 14:13:57 +0800

    加入圖床Cloudinary Api與測試範例

--- Web/Program.cs
+++ Web/Program.cs
@@ -38,6 +38,7 @@ namespace Web
             builder.Services.AddScoped<AccountService>();
             builder.Services.AddScoped<NationService>();
             builder.Services.AddScoped<CourseCategoryService>();
+            builder.Services.AddScoped<CloudinaryService>();
 
 
 
