dc8195bd2154934578df4c52dc90f6836a39b45a
Chiaki <dark0423@hotmail.com>
2024-09-17 14:34:29 +0800

    調整: ITransactionAsync及其相關介面與實作

--- ApplicationCore/Interfaces/ITransaction.cs
+++ ApplicationCore/Interfaces/ITransaction.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ApplicationCore.Interfaces
+﻿namespace ApplicationCore.Interfaces
 {
-    public interface ITransaction : ITransActionAsync
+    public interface ITransaction : ITransactionAsync
     {
         void BeginTransAction();
         void Commit();
