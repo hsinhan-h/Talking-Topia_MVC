0d112a349df6aab0f2490f12ae67b3a04444cc07
XieLiLun <don21471105@gmail.com>
2024-09-18 17:57:57 +0800

    member Controller帶ID

--- Web/Controllers/MemberController.cs
+++ Web/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using Web.Entities;
+﻿using ApplicationCore.Entities;
+using Web.Entities;
 using Web.Services;
 
 namespace Web.Controllers
@@ -7,20 +8,38 @@ namespace Web.Controllers
     {
         private readonly MemberDataService _memberDataService;
         private readonly OrderDetailService _orderDetailService;
+        private readonly MemberAppointmentService _memberAppointmentService;
 
-        public MemberController(MemberDataService memberDataService,OrderDetailService orderdetailservice)
+        public MemberController(MemberDataService memberDataService,OrderDetailService orderdetailservice, MemberAppointmentService memberappointmentService)
         {
             _memberDataService = memberDataService;
             _orderDetailService = orderdetailservice;
+            _memberAppointmentService = memberappointmentService;
         }
         /// <summary>
         /// 原MemberCenterHomepage.cshtml頁面
         /// 調整為學員課程預約明細
         /// </summary>
         /// <returns></returns>
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var memberId = 15; // 測試使用 MemberId
+            var viewModel = await _memberAppointmentService.GetAppointmentData(memberId);
+
+            // 確保 viewModel 被正確初始化
+            if (viewModel == null)
+            {
+                viewModel = new MemberAppointmentViewModel
+                {
+                    MemberAppointmentList = new List<MemberAppointmentVM>() // 初始化為空列表
+                };
+            }
+            else if (viewModel.MemberAppointmentList == null)
+            {
+                viewModel.MemberAppointmentList = new List<MemberAppointmentVM>(); // 確保列表不為 null
+            }
+
+            return View(viewModel); // 將正確初始化的 viewModel 傳遞到視圖
         }
         public async Task<IActionResult> MemberData(int memberId)
         {
