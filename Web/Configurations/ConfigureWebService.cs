76aa193dfbc56608facdaded3e1805c6fb9e8c03
XieLiLun <don21471105@gmail.com>
2024-09-18 17:57:02 +0800

    add MemberAppointmentService

--- Web/Configurations/ConfigureWebService.cs
+++ Web/Configurations/ConfigureWebService.cs
@@ -13,6 +13,7 @@ namespace Web.Configurations
             services.AddScoped<ShoppingCartViewModelService>();
             services.AddScoped<AppointmentDetailService>();
             services.AddScoped<OrderDetailService>();
+            services.AddScoped<MemberAppointmentService>();
             return services;
         }
     }
