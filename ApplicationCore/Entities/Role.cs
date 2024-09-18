2a1b736ec7df6d2e219b01dc9c24b540cbd26031
Yu Chun Wu <wu.960813@gmail.com>
2024-09-18 18:17:23 +0800

    Entities + User/Role/UserRole

---
+++ ApplicationCore/Entities/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public RoleType RoleType { get; set; }
+        public ICollection<UserRole> UserRole { get; set; }
+    }
+    public enum RoleType
+    {
+        Normal = 0, // 一般會員
+        Tutor = 1, // 教師
+        Admin = 2 // 系統管理者
+    }
+}
---
+++ ApplicationCore/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<UserRole> UserRole { get; set; }
+
+        // 一對一關聯，User 對應一個 Member
+        public virtual Member Member { get; set; }
+    }
+}
