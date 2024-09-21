a7a2c35ba06589f65b3a987858f1d47d7ae07a19
Hsin Han <hsinhan831102@gmail.com>
2024-09-21 23:42:06 +0800

    優化youtubeModal.js

--- Web/wwwroot/js/course_list/course_list_youtubeModal.js
+++ Web/wwwroot/js/course_list/course_list_youtubeModal.js
@@ -1,11 +1,13 @@
 ﻿//youtube彈出視窗
 export function autoPlayYouTubeModal() {
-    var trigger = $("body").find('[data-bs-toggle="modal"]');
+    var trigger = $("body").find('[data-bs-toggle="modal"][data-bs-target="#videoModal"]');
     trigger.click(function () {
         var theModal = $(this).data("bs-target"),
             videoSRC = $(this).attr("data-bs-video"),
             videoSRCauto = videoSRC + "?autoplay=1";
         $(theModal + " iframe").attr("src", videoSRCauto);
+
+        //彈出視窗關閉時停止撥放
         $(theModal).on("hidden.bs.modal", function () {
             $(theModal + " iframe").attr("src", "");
         });
