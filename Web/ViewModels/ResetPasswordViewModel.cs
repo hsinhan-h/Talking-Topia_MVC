2366838e741110bfb0a41f5d88d10ab2f2f11453
Yu Chun Wu <wu.960813@gmail.com>
2024-09-28 01:47:27 +0800

    feat: ForgotPassword and ResetPassword OK

---
+++ Web/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Token { get; set; }
+
+        [Required(ErrorMessage = "請輸入新密碼")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "請再次輸入新密碼")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "密碼不一致")]
+        public string ConfirmPassword { get; set; }
+    }
+}
