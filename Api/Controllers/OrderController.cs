c456a5014d4f821fc46513ce74f0a0a672578c2f
Chiaki <dark0423@hotmail.com>
2024-10-03 03:33:57 +0800

    feat: Api.Order Module(Update功能未寫完)

--- Api/Controllers/OrderController.cs
+++ Api/Controllers/OrderController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Api.Dtos;
+using Api.Services;
+using ApplicationCore.Entities;
+using ApplicationCore.Enums;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -7,8 +12,37 @@ namespace Api.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-       
+        private readonly OrderApiService _orderApiService;
 
+        public OrderController(OrderApiService orderApiService)
+        {
+            _orderApiService = orderApiService;
+        }
 
+        /// <summary>
+        /// 撈取Order
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var result = await _orderApiService.GetAllOrders();
+
+            return Ok(new BaseApiResponse(result));
+        }
+
+        /// <summary>
+        /// 更新Order
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrder(UpdateOrderDto request)
+        {
+            var result =  await _orderApiService.UpdateOrder(request);
+
+            if (result == 1 ) return Ok(200);
+            else return BadRequest("更新失敗");
+        }
     }
 }
