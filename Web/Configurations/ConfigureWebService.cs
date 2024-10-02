723424b9e7fd0f773563545491b9cb19b089c163
Chiaki <dark0423@hotmail.com>
2024-10-02 21:00:11 +0800

    feat: 會員訂單功能

--- Web/Configurations/ConfigureWebService.cs
+++ Web/Configurations/ConfigureWebService.cs
@@ -6,7 +6,7 @@
         {
             services.AddScoped<ShoppingCartViewModelService>();
             services.AddScoped<AppointmentDetailService>();
-            services.AddScoped<OrderDetailService>();
+            services.AddScoped<OrderDetailViewModelService>();
             services.AddScoped<OrderViewModelService>();
             services.AddScoped<MemberAppointmentService>();
             //services.AddScoped<DifySearchService>();
