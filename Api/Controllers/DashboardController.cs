c503f2132a2b8b08c0337e79245cc989f396e7c1
Yu Chun Wu <wu.960813@gmail.com>
2024-10-06 23:01:10 +0800

    add DashboardController

---
+++ Api/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize]  // 只有持有有效 JWT 的使用者才能訪問
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetDashboard()
+        {
+            return Ok(new { message = "Welcome to the protected dashboard!" });
+        }
+    }
+}
