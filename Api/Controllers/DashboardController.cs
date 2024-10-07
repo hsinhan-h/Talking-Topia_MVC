8761baccc729d40fa53bb1d828d0907891f38120
Chiaki <dark0423@hotmail.com>
2024-10-07 23:08:39 +0800

    更新: DashboardController內容及ApiService(尚未寫邏輯)

--- Api/Controllers/DashboardController.cs
+++ Api/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -8,10 +9,25 @@ namespace Api.Controllers
     [ApiController]
     public class DashboardController : ControllerBase
     {
+        private readonly DashboardApiService _dashboardApiService;
+
+        public DashboardController(DashboardApiService dashboardApiService)
+        {
+            _dashboardApiService = dashboardApiService;
+        }
+
         [HttpGet]
         public IActionResult GetDashboard()
         {
             return Ok(new { message = "Welcome to the protected dashboard!" });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboardData()
+        {
+            var result = await _dashboardApiService.GetAllData();
+
+            return Ok(new BaseApiResponse(result));
         }
     }
 }
