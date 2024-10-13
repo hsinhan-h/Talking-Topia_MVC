5a7f49990b8827178e87702f7689f9c8e5a26461
Michael <m051412369811@gmail.com>
2024-10-13 18:56:55 +0800

    關注列表修改中(API寫完)

---
+++ Web/Controllers/Api/TutorCardController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+namespace Web.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TutorCardController : ControllerBase
+    {
+        private readonly CourseService _courseService;
+        private readonly MemberDataService _memberdataService;
+
+
+        public TutorCardController(CourseService courseService, MemberDataService memberdataService)
+        {
+            _courseService = courseService;
+            _memberdataService = memberdataService;
+        }
+
+        [HttpGet]
+        public IActionResult GetRecommandTutorCard([FromQuery]int categoryId)
+        {
+            var getCardData = _courseService.GetTutorRecommendCard(categoryId);
+            var result = JsonConvert.SerializeObject(getCardData);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetWatchListCard()
+        {
+            var memberIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+            int memberId = int.Parse(memberIdClaim.Value);
+
+            var getCardData = _memberdataService.GetWatchList(memberId);
+            var result = JsonConvert.SerializeObject(getCardData);
+            return Ok(result);
+        }
+    }
+}
