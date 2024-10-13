1369650b0deecf2526945a16b7b6e086bb71e287
Yu Chun Wu <wu.960813@gmail.com>
2024-10-13 16:30:32 +0800

    Add required field validation messages to the register page

--- Web/ViewModels/RegisterViewModel.cs
+++ Web/ViewModels/RegisterViewModel.cs
@@ -2,21 +2,21 @@
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "電子郵件是必填欄位")]
+        [Required(ErrorMessage = "電子郵件是必填的")]
         [EmailAddress(ErrorMessage = "請輸入有效的電子郵件地址")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "密碼是必填欄位")]
+        [Required(ErrorMessage = "密碼是必填的")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required(ErrorMessage = "請確認您的密碼")]
+        [Required(ErrorMessage = "確認密碼是必填的")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "兩次密碼不一致")]
         public string ConfirmPassword { get; set; }
-        [Required(ErrorMessage = "名字是必填欄位")]
+        [Required(ErrorMessage = "名字是必填的")]
         [StringLength(50, ErrorMessage = "名字不得超過50字元")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "姓氏是必填欄位")]
+        [Required(ErrorMessage = "姓氏是必填的")]
         [StringLength(50, ErrorMessage = "名字不得超過50字元")]
         public string LastName { get; set; }
 
