45ec8e0a33a7c4470c5c5306b97017a4a8a3b306
Michael <m051412369811@gmail.com>
2024-10-26 12:36:54 +0800

    新增評論審查功能，新增mongoDBVectorSearch appsettings

--- ApplicationCore/Interfaces/IOpenAIService.cs
+++ ApplicationCore/Interfaces/IOpenAIService.cs
@@ -1,5 +1,4 @@
-﻿using ApplicationCore.Dtos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Interfaces
 {
-    public interface ICourseService
+    public interface IOpenAIService
     {
-        public bool IsCourse(int courseId);
+        public Task<string> ModerateContentAsync(string inputText);
 
 
     }
